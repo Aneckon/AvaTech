c0dedd97f556dda8376971b9b67f42698f49677d
Aneckon <83111616+Aneckon@users.noreply.github.com>
2021-08-10 12:37:24 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -6,6 +6,24 @@ $(document).ready(function() {
     });
 });
 
+$(document).ready(function() {
+  $('.ua').click(function() {
+      $('.countryitem').hide();
+      $('.ukraina__item').show();
+  });
+  $('.gr').click(function() {
+    $('.countryitem').hide();
+    $('.germania__item').show();
+  });
+  $('.bl').click(function() {
+    $('.countryitem').hide();
+    $('.belarus__item').show();
+  });
+  $('.pl').click(function() {
+    $('.countryitem').hide();
+    $('.pol__item').show();
+  });
+});
 
 
 
@@ -19,8 +37,67 @@ var swiper = new Swiper(".mySwiper", {
     pagination: {
       el: ".swiper-pagination",
        },
-    history: {
-      key: "slide",
-    },
 });
 
+
+
+var number = document.querySelector('.number'),
+		numberTop = number.getBoundingClientRect().top,
+		start = +number.innerHTML, end = +number.dataset.max;
+
+window.addEventListener('scroll', function onScroll() {
+	if(window.pageYOffset > numberTop - window.innerHeight / 2) {
+		this.removeEventListener('scroll', onScroll);
+		var interval = setInterval(function() {
+			number.innerHTML = ++start;
+			if(start == end) {
+				clearInterval(interval);
+				}
+		}, 60);
+	}
+});
+var number1 = document.querySelector('.number1'),
+		number1Top = number1.getBoundingClientRect().top,
+		start1 = +number1.innerHTML, end1 = +number1.dataset.max;
+
+		window.addEventListener('scroll', function onScroll() {
+			if(window.pageYOffset > number1Top - window.innerHeight / 2) {
+				this.removeEventListener('scroll', onScroll);
+				var interval1 = setInterval(function() {
+					number1.innerHTML = ++start1;
+					if(start1 == end1) {
+						clearInterval(interval1);
+					}
+				}, 60);
+			}
+});
+var number2 = document.querySelector('.number2'),
+		number2Top = number2.getBoundingClientRect().top,
+		start2 = +number2.innerHTML, end2 = +number2.dataset.max;
+
+		window.addEventListener('scroll', function onScroll() {
+			if(window.pageYOffset > number2Top - window.innerHeight / 2) {
+				this.removeEventListener('scroll', onScroll);
+				var interval2 = setInterval(function() {
+					number2.innerHTML = ++start2;
+					if(start2 == end2) {
+						clearInterval(interval2);
+					}
+				}, 60);
+			}
+});
+var number3 = document.querySelector('.number3'),
+		number3Top = number3.getBoundingClientRect().top,
+		start3 = +number3.innerHTML, end3 = +number3.dataset.max;
+
+		window.addEventListener('scroll', function onScroll() {
+			if(window.pageYOffset > number3Top - window.innerHeight / 2) {
+				this.removeEventListener('scroll', onScroll);
+				var interval3 = setInterval(function() {
+					number3.innerHTML = ++start3;
+					if(start3 == end3) {
+						clearInterval(interval3);
+					}
+				}, 1);
+			}
+});
